0beba27f24579df9870e711fd958140a7b36d296
youchen624 <youchen624@gmail.com>
2025-12-01 10:47:46 +0800

    w13Mp1

---
+++ QT_Project_Plants_vs_Zombies_absver_1201/GameCore.cpp
@@ -0,0 +1,3 @@
+#include "GameCore.h"
+
+GameCore::GameCore() {}
---
+++ QT_Project_Plants_vs_Zombies_absver_1201/GameCore.h
@@ -0,0 +1,12 @@
+#ifndef GAMECORE_H
+#define GAMECORE_H
+
+#include <QWidget>
+
+class GameCore
+{
+public:
+    GameCore();
+};
+
+#endif // GAMECORE_H
